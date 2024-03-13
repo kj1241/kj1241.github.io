280940429e465bdb5247e7d1faddf743655ced27
kj1241 <kyeoung1241@gmail.com>
2024-03-14 04:33:32 +0900

    modify github blog in web

--- _posts/Toy Porjects/Git_Hub_blog/2023-11-23-Git_Hub_Blog_Skin.markdown
+++ _posts/Toy Porjects/Git_Hub_blog/2023-11-23-Git_Hub_Blog_Skin.markdown
@@ -1,144 +1,206 @@
 ---
 layout: post
-title:  "깃 허브 블로그 스킨 제작 v0.3"
-date:   2023-11-23 09:00:00 +0900
-image: https://drive.google.com/uc?id=1Q-Dt-nV182c12d9Yt_55m-2QJnlttXJs
+title: "깃 허브 블로그 스킨 제작 v0.4"
+date: 2023-11-23 09:00:00 +0900
+image: https://github.com/kj1241/kj1241.github.io/assets/22047442/bd645704-5970-41e8-a8c7-52f0784b2cba
 toc: true
 categories: [Git_Hub_Blog]
 tags: [HTML, SCSS, Java script, Ruby, Jekyll, Git Hug Blog]
 ---
 
+##  <web_h2> 프로젝트 소개 </web_h2>
 
-##  <green1_h2> 토이 프로젝트 소개 </green1_h2>
-
-우주적 감각을 담은 심플한 디자인으로 눈길을 사로잡는 우주 테마 스킨.  
-미니멀한 인터페이스와 고요한 색감으로 집중력을 높여줍니다.  
+우주적 감각을 담은 심플한 디자인으로 눈길을 사로잡는 우주 테마 스킨입니다.  
+미니멀한 인터페이스와 고요한 색감으로 집중력과 시각적 효과를 증가시켜줍니다.   
 단순함 속에 심장을 뛰게 하는 우주적 아름다움을 느껴보세요.
 
 <br>
-<br>
 
-## <green1_h2> 토이 프로젝트 개요 </green1_h2>
+## <web_h2> 프로젝트 개요 </web_h2>
 
-- <span><green1_h5>프로젝트명: </green1_h5> KJ_Skin v0.3 </span>
-- <span><green1_h5>장르: </green1_h5> Toy Project(Blog Theme) </span>
-- <span><green1_h5>기간: </green1_h5> 2023.11.18~2023.11.20(v0.1) / 2023.11.25(v0.2) /2023.12.02~2023.12.03(v0.3) </span>
-- <span><green1_h5>플랫폼: </green1_h5> PC(Chorm / Microsoft Edge / Firefox / Opera)</span>
-
-
-<br>
-<br>
-
-## <green1_h2> 팀 구성 </green1_h2>
-
-- <span><green1_h5>Developer(1명): </green1_h5>블로그 UI화면 구성, 슬라이더 제작, 버튼제작, TOC제작 </span>
+- <p><web_h5>프로젝트명:</web_h5> KJ_Skin v0.4</p>
+- <p><web_h5>장르:</web_h5> Toy Project(Blog Theme)</p>
+- <p><web_h5>기간:</web_h5> 운영중</p>
+    - v0.1: 2023.11.18~2023.11.20 
+    - v0.2: 2023.11.25  
+    - v0.3: 2023.12.02~2023.12.03 
+    - v0.4: 2024.03.11~2024.03.13 
+- <p><web_h5>개발인원:</web_h5> Developer(1명)</p>
+- <p><web_h5>플랫폼:</web_h5> PC(Chorm / Microsoft Edge / Firefox / Opera)</p>
 
 
 <br>
-<br>
 
-## <green1_h2> 기술 스택 </green1_h2>
+### <web_h3> 기술 스택 </web_h3>
 
-- <span><green1_h5>엔진: </green1_h5> visual code </span>
-- <span><green1_h5>언어:  </green1_h5> Java script / Ruby / HTML / SCSS / jekyll </span>
-- <span><green1_h5>그래픽 디자인: </green1_h5> Adobe Photoshop< </span>
+- <p><web_h5>개발 도구:</web_h5> visual code / Git hub</p>
+- <p><web_h5>개발 언어:</web_h5> HTML / SCSS / JavaScript / Jekyll / Markdown / Ruby</p>
+- <p><web_h5>그래픽 디자인:</web_h5> Adobe Photoshop</p>
 
 
 <br>
 <br>
 
-## <green1_h2> 토이 프로젝트 특징 </green1_h2>
+## <web_h2> 프로젝트 특징 및 기능 구현 </web_h2>
 
-1. 슬라이더 애니메이션을 구현하여 홈페이지에 좀 더 생동감을 불어 넣었습니다.
-2. 단순하고 심플한 느낌이 들 수 있도록 화면을 구상하였습니다.
+업데이트 별 프로젝트 특징과 기능 구현 내용입니다.
+
+### <web_h3>단순형 스킨 제작 v0.1</web_h3><red1_error> (전체 제작 기여도: 100%)</red1_error>
+
+1. 라이브러리 없이 테마를 처음부터 제작하였습니다.
+2. 홈 커버에 슬라이더 애니메이션을 제작하여 추가하였습니다.
+3. 카테고리를 제작하였습니다.
+4. 목차를 제작하였습니다.
+
+<br>
+
+#### <web_h4>1) 홈커버 화면</web_h4>
+
+![깃허브블로그 홈커버]({{ site.google_drive }}1OzqLMBVTtXSBCgmB8SjPgV9n9eaGn61Q{{ site.google_drive_end }}){:width="100%" height="auto" loading="lazy"}
+
+- JavaScript 사용하여 라이브러리 없이 직접 슬라이더를 제작하였습니다.
+- 홈 커버에 슬라이더를 만들고 생동감을 부여하였습니다.  
+
+<br>
+
+#### <web_h4>2) 카테고리 페이지 화면</web_h4>
+
+![깃허브블로그 홈커버]({{ site.google_drive }}1Q9YyrCUtPyOfZSDsuwRZeajwHBsGD3_q&{{ site.google_drive_end }}){:width="100%" height="auto" loading="lazy"}
+
+- HTML + SCSS를 이용하여 카테고리 제작하였습니다.
+- 카테고리를 제작하여 사용자가 편하게 제작할 수 있도록 구성하였습니다.
+- Jekyll를 이용하여 페이지 화면을 구성하였습니다.
+
+<br>
+
+#### <web_h4>3) 포스터 화면 및 목차</web_h4>
+
+![깃허브 블로그 포스터]({{ site.google_drive }}1QAh73nYwJTAb3UOWDCYgyBkK9tYn1Z0Y{{ site.google_drive_end }}){:width="100%" height="auto" loading="lazy"}
+
+- JavaScript 사용하여 동적으로 목차를 생성하게 코드 작성하였습니다.
+- 목차를 누르면 해당 페이지 위치로 이동합니다.
+- Jekyll를 이용하여 포스터 화면을 제작하였습니다.
+
+<br>
+
+### <web_h3>모바일 스킨 제작 v0.2</web_h3><red1_error> (전체 제작 기여도: 100%)</red1_error>
+
+1. 모바일 버전을 제작하기 위해서 반응형, 적응형 디자인 둘을 섞어서 만들었습니다.
+    + 메인 콘텐츠의 박스는 웹페이지 크기로 반응형입니다.
+    + 1350px 이상은 사이드바가, 1350px 이하는 header가 만들어지는 적응형입니다.
+2. 모바일용 카테고리 헤더를 제작하였습니다.
+
+<br>
+
+#### <web_h4>4) 반응형& 적응형 디자인</web_h4>
+
+![반응형&적응형 디자인]({{ site.google_drive }}1bdHRggPHlKQLcHW0L_2eIwwy12idXyJI{{ site.google_drive_end }}){:width="100%" height="auto" loading="lazy"}
+
+- HTML + SCSS를 이용하여 반응형 및 적응형 디자인을 제작하였습니다.
+- 1350px 이상은 사이드바, 1350px 이하는 header가 만들어지는 적응형으로 만들어집니다.
+
+<br>
+
+#### <web_h4>5) 모바일용 카테고리 헤더 제작</web_h4>
+
+![모바일용 카테고리 헤더]({{ site.google_drive }}1vQRTrNEeIzGEkuvmKGb15Uq4sr9VDw8d{{ site.google_drive_end }}){:width="100%" height="auto" loading="lazy"}
+
+- Jekyll를 이용하여 버튼 이벤트를 만들었습니다.
+- HTML + SCSS 카테고리 코드를 사용하였습니다.
+- svg를 이용하여 간편한 심볼을 넣었습니다.
+
+<br>
+
+### <web_h3>최적화 설정 v0.3</web_h3><red1_error> (전체 제작 기여도: 100%)</red1_error>
+
+1. tilte, description, keywords를 설정 및 구현하였습니다.
+2. seo-tag 설정하여 보다 검색에 친숙하게 구현하였습니다.
+3. robots.txt, sitmap.xml 설정하여 구글 서치콘솔 및 빙 웹마스터에 연동하였습니다.
+4. 파비콘 추가하여 사용자 경험을 확대하였습니다.
+5. 각종 최적화 진행하였습니다. ( png → webp 변경등 )
+
+
+<br>
+
+### <web_h3>에러해결 및 리뉴얼 v0.3</web_h3><red1_error> (전체 제작 기여도: 100%)</red1_error>
+
+1. 웹의 분위기를 환기하고자 폰트를 바꾸었습니다.
+2. 구글 정책 변경에 따라서 [이미지 호스팅](/_posts/Technical_Article/Web/2024-03-01-GoogleDriveImageEmbed%20.markdown)을 바꾸고 매크로를 작성하였습니다.
+
+<br>
+
+#### <web_h4>6) 웹 폰트 변경</web_h4>
+
+![웹 폰트 변경]({{ site.google_drive }}1wJPMRBKT77DCekpcagAlxd5Ahy4YmtJu{{ site.google_drive_end }}){:width="100%" height="auto" loading="lazy"}
+
+- 넥슨[배찌체], 네이버[부장님 눈치체], 한국교육학술정보원[학교 안심 봄방학체], 구글 폰트[Whisper, WindSong] 폰트를 사용하였습니다.
+- 폰트를 바꿈으로서 분위기를 환기하였습니다.
+
+<br>
+
+### <web_h3>제작 후 폐기된 코드들</web_h3><red1_error> (전체 제작 기여도: 100%)</red1_error>
+
+가독성에 이유로 다음과 같은 코드들이 폐기되었습니다.
+
+1. 백그라운드에서 유성이 떨어지는 효과가 폐기되었습니다.
+2. 마우스를 쫒차다니면서 시간을 알려주던 효과가 폐기되었습니다.
+
+
+<br>
+
+#### <web_h4>7) 백그라운드 유성 효과</web_h4>
+
+![유성 효과]({{ site.google_drive }}1nJaErEEOcZOi2bR5ZdwA7QyiiHLi-kCF{{ site.google_drive_end }}){:width="100%" height="auto" loading="lazy"}
+
+- JavaScript를 사용하여 동적으로 유성이 떨어지는 효과를 제작하였습니다.
+- HTML + SCSS를 유성을 만들었습니다.
+
+<br>
+
+#### <web_h4>8) 마우스를 따라 다니는 시계 효과</web_h4>
+
+![마우스를 따라 다니는 시계 효과]({{ site.google_drive }}1fPY3vhUusIWtjalKpuLCnZomHraqlYbM{{ site.google_drive_end }}){:width="100%" height="auto" loading="lazy"}
+
+- JavaScript를 사용하여 마우스 위치찾아서 시계가 따라다니는 효과를 제작하였습니다.
+
 
 <br>
 <br>
 
-## <green1_h2> 개발자의 역활 및 경험 </green1_h2>
+## <web_h2> 프로그래밍 로직 및 파일 구조 </web_h2>
 
-- **단순형 스킨 제작 v0.1** <red1_error>(전체 게임 제작 기여도: 100%)</red1_error>
-    1. 테마를 처음부터 제작하기 위해 블로그 프로그래밍 구조를 설계.
-    2. 홈 커버에 슬라이더 에니메이션을 제작.
-    3. 카테고리를 제작하여 사용자가 편하게 접근 할 수 있도록 제작.
-    4. 목차를 효율적으로 볼 수 있도록 TOC를 제작.
+![웹페이지 구조]({{ site.google_drive }}10UrW2c4mn_YSR_bZ9kh1X2ZEsz9FB4Qd{{ site.google_drive_end }}){:width="100%" height="auto" loading="lazy"}
 
-- **모바일 스킨 제작 v0.2** <red1_error>(전체 게임 제작 기여도: 100%)</red1_error>
-    1. 반응형, 적응형 디자인 둘을 섞음.
-        + 메인 콘텐츠의 박스는 웹페이지 크기로 반응형으로 디자인.
-        + 1350px 이상은 사이드바가, 1350px 이하는 header가 만들어지는 적응형으로 디자인.
-    2. 모바일에서 글 카테고리를 보여주기 위해 header를 제작.
-
-- **최적화 및 Seo-tag 설정 v0.3** <red1_error>(전체 게임 제작 기여도: 100%)</red1_error>
-    1. tilte, description, keywords를 설정.
-    2. seo-tag 설정
-    3. robots.txt, sitmap.xml 설정
-    4. 파비콘 추가
-    5. 각종 최적화 진행 ( png → webp 변경등 )
-
-
-
-<br>
-
-### <green1_h3>프로그래밍 파일 구조</green1_h3> 
-- HTML + SCSS + JS + Ruby + Jekyll
- <!--이미지 못침범하게-->
-<p><green1_h5>데이터 구조 </green1_h5></p>
-![깃허브_블로그_데이터구조](https://drive.google.com/uc?export=view&id=1PwrHSlIjMBSov3P-5bvGQYbyI4wsmnmB){:width="100%" height="auto" loading="lazy"}
-<p><green1_h5>웹 페이지 구조</green1_h5> </p>
-![blog글 구조](https://drive.google.com/uc?export=view&id=Pyo_Q9YbDYhq5oWG4EdRVgiZc9opj095){:width="100%" height="auto" loading="lazy"}
-
-- YML: 데이터 구조를 담당하고 있습니다.
-- HTML: 화면 구성의 구조를 담당하고 있습니다.
-- CSS: 화면 구성의 스타일을 담당하고 있습니다.
-- JS: 화면의 이벤트를 담당하고 있습니다.
-- Jekyll: 사이트 정적 웹 툴입니다.
-- Ruby: 사이트의 플러그인과 liquid 동적 콘텐츠를 생상합니다.
-
-
-<br>
-
-### <green1_h3> 홈커버 화면</green1_h3>
-
-![깃허브블로그 홈커버](https://drive.google.com/uc?export=view&id=1Q-Dt-nV182c12d9Yt_55m-2QJnlttXJs){:width="100%" height="auto" loading="lazy"}
-
-<br>
-
-### <green1_h3> 카테고리 페이지 화면</green1_h3>
-
-![깃허브블로그 페이지](https://drive.google.com/uc?export=view&id=1Q9YyrCUtPyOfZSDsuwRZeajwHBsGD3_q){:width="100%" height="auto" loading="lazy"}
-
-<br>
-
-### <green1_h3> 포스트 화면</green1_h3>
-
-![깃허브 블로그 포스트](https://drive.google.com/uc?export=view&id=1QAh73nYwJTAb3UOWDCYgyBkK9tYn1Z0Y){:width="100%" height="auto" loading="lazy"}
+- 머리속에 있는 프로그래밍 로직 구조를 도식화 시킨 그림입니다.
 
 <br>
 <br>
 
-## <green1_h2> 결과(성과) 및 데모 </green1_h2>
+## <web_h2> 결과(성과) 및 데모 </web_h2>
 
-![깃허브블로그 평가](https://drive.google.com/uc?export=view&id=1QJ617PH-_g2y71uz4FUDYXht1VbCtEKX){:width="100%" height="auto" loading="lazy"}
+![깃허브블로그 평가]({{ site.google_drive }}1QJ617PH-_g2y71uz4FUDYXht1VbCtEKX{{ site.google_drive_end }}){:width="100%" height="auto" loading="lazy"}
 
-- <span><green1_h5>성과: </green1_h5>pc버전 단순형 스킨 제작 </span>
-- <span><green1_h5>깃 허브(코드): </green1_h5> [https://github.com/kj1241/kj1241.github.io](https://github.com/kj1241/kj1241.github.io) </span>
+- <p><web_h5>성과:</web_h5> 웹 페이지 PC 버전 및 모바일 버전 제작 </p>
+- <p><web_h5>깃 허브(코드):</web_h5> [https://github.com/kj1241/kj1241.github.io](https://github.com/kj1241/kj1241.github.io)</p>
 
 
 <br>
+<br>
 
-### <green1_h3> 비고 및 여담 </green1_h3>
+## <web_h2> 비고 및 여담 </web_h2>
 
-- **깃 허브 블로그로 옮긴 이유:**
+- <p><web_h5>깃 허브 블로그로 옮긴 이유:</web_h5></p>
     - 티스토는 UI부터 초보자가 사용하기 좋은 사이트임에는 틀림없지만, 원하는대로 만들수 없는 사이트이기 때문에 저의 니즈를 충족시키지 못하여서 옮기게 되었습니다.
 
-- **깃 허브 제작시 트러블 슈팅:**
-    - jekyll에서 _Page폴더를 만들고 내용을 읽어 올 수 없습니다. <highlight_orange>따라서 pages라는 폴더로 만들었습니다.</highlight_orange>
-    - Github renderer에서 color의 값으로 RGB를 사용해서 색을 지정하면 커밋에서 빌드에러를 일으킵니다. <highlight_orange>따라서 color 값을 "#" 형식인 16진수로 적어야 합니다.</highlight_orange>~~( erorr 보고서도 없고 어디가 틀렸는지도 안나오고, 실험하느라 Repository 한개 날려먹음...)~~
+- <p><web_h5>깃 허브 제작시 트러블 슈팅:</web_h5></p>
+    - jekyll에서 _Page폴더를 만들고 내용을 읽어 올 수 없습니다. 따라서 pages라는 폴더로 만들었습니다.
+    - 구글 이미지 호스팅 주소가 변경됨에 따라 주소를 변경하고 매크로를 제작하였습니다.
 
 <br>
 
 ### <green1_h3> 배운 점 및 앞으로 수정할 점 </green1_h3>
 
 - Ruby와 jekyll를 사용해서 어떻게 테마를 제작는지 학습하는 시간이 되었습니다.
-- 나중에 여유가 된다면 반응형 디자인으로 모바일도 지원할 계획이 있습니다.
+- 나중에 여유가 된다면 반응형 디자인으로 모바일도 지원할 계획이 있습니다. (완료)
+- 백그라운드 애니메이션과 마우스 이펙트 효과를 주고 싶습니다. (폐기)
+
--- assets/js/mouse.js
+++ assets/js/mouse.js
@@ -1,94 +1,240 @@
-var colour="#639"; //시계 색상
-var clock_size=5; //시계 크기
+// var colour="#639"; //시계 색상
+// var clock_size=10; //시계 크기
 
-var clok;
-var cpos=0;
-var newx=xmo=400;
-var newy=ymo=300;
-window.onload=function() { if (document.getElementById) {
-  clok=document.createElement("div");
-  clok.style.position="absolute";
-  clok.appendChild(createDigit("dig0"));
-  clok.appendChild(createDigit("dig1"));
-  clok.appendChild(createColon());
-  clok.appendChild(createDigit("dig2"));
-  clok.appendChild(createDigit("dig3"));
-  clok.appendChild(createColon("ms"));
-  clok.appendChild(createDigit("dig4"));
-  clok.appendChild(createDigit("dig5"));
-  document.body.appendChild(clok);
-  tick();
-}}
+// var clok;
+// var cpos=0;
+// var newx=xmo=400;
+// var newy=ymo=300;
+// window.onload=function() { if (document.getElementById) {
+//   clok=document.createElement("div");
+//   clok.style.position="absolute";
+//   clok.appendChild(createDigit("dig0"));
+//   clok.appendChild(createDigit("dig1"));
+//   clok.appendChild(createColon());
+//   clok.appendChild(createDigit("dig2"));
+//   clok.appendChild(createDigit("dig3"));
+//   clok.appendChild(createColon("ms"));
+//   clok.appendChild(createDigit("dig4"));
+//   clok.appendChild(createDigit("dig5"));
+//   document.body.appendChild(clok);
+//   tick();
+// }}
 
-var digit=new Array(247, 146, 221, 219, 186, 235, 239, 210, 255, 251);
+// var digit=new Array(247, 146, 221, 219, 186, 235, 239, 210, 255, 251);
 
-function tick() {
-  var now=new Date();
-  var t=Math.floor(now.getTime()/500)%2;
-  var i, j, k;
-  var won=oo(now.getHours())+oo(now.getMinutes())+oo(now.getSeconds());
-  for (i=0; i<6; i++) for (j=0; j<7; j++) {
-	k=digit[won.substring(i, i+1)].toString(2).substring(j+1, j+2);
-	document.getElementById("dig"+i+j).style.backgroundColor=(k==1)?colour:"transparent";
-  }
-  document.getElementById("mstop").style.backgroundColor=(t)?"transparent":colour;
-  document.getElementById("msbot").style.backgroundColor=(t)?"transparent":colour;
-  xmo+=Math.floor((newx-xmo)/4);
-  clok.style.left=Math.round(xmo-(clock_size+2)*4)+"px";
-  ymo+=Math.floor((newy-ymo)/4);
-  clok.style.top=Math.round(ymo-clock_size*2.5)+"px";
-  setTimeout("tick()", 40);
-}
+// function tick() {
+//   var now=new Date();
+//   var t=Math.floor(now.getTime()/500)%2;
+//   var i, j, k;
+//   var won=oo(now.getHours())+oo(now.getMinutes())+oo(now.getSeconds());
+//   for (i=0; i<6; i++) for (j=0; j<7; j++) {
+// 	k=digit[won.substring(i, i+1)].toString(2).substring(j+1, j+2);
+// 	document.getElementById("dig"+i+j).style.backgroundColor=(k==1)?colour:"transparent";
+//   }
+//   document.getElementById("mstop").style.backgroundColor=(t)?"transparent":colour;
+//   document.getElementById("msbot").style.backgroundColor=(t)?"transparent":colour;
+//   xmo+=Math.floor((newx-xmo)/4);
+//   clok.style.left=Math.round(xmo-(clock_size+2)*4)+"px";
+//   ymo+=Math.floor((newy-ymo)/4);
+//   clok.style.top=Math.round(ymo-clock_size*2.5)+"px";
+//   setTimeout("tick()", 40);
+// }
 
-document.onmousemove=mouse;
+// document.onmousemove=mouse;
 
-function mouse(e) {
-  newy=(e)?e.pageY:event.y+scro();
-  newx=(e)?e.pageX:event.x;
-}
+// function mouse(e) {
+//   newy=(e)?e.pageY:event.y+scro();
+//   newx=(e)?e.pageX:event.x;
+// }
 
-function scro() {
-  var scro=0;
-  if (document.body.scrollTop) scro=document.body.scrollTop;
-  else if (document.documentElement && document.documentElement.scrollTop) scro=document.documentElement.scrollTop;
-  return (scro);
-}
+// function scro() {
+//   var scro=0;
+//   if (document.body.scrollTop) scro=document.body.scrollTop;
+//   else if (document.documentElement && document.documentElement.scrollTop) scro=document.documentElement.scrollTop;
+//   return (scro);
+// }
 
-function oo(o) { return(((o<10)?"0"+o:o).toString()); }
+// function oo(o) { return(((o<10)?"0"+o:o).toString()); }
 
-function createDigit(id) {
-  var odiv=createDiv(0, cpos, clock_size+clock_size+2, clock_size+2, false);
-  cpos+=clock_size+4;
-  odiv.appendChild(createDiv(0, 1, 2, clock_size, id+"0"));
-  odiv.appendChild(createDiv(1, 0, clock_size, 2, id+"1"));
-  odiv.appendChild(createDiv(1, clock_size, clock_size, 2, id+"2"));
-  odiv.appendChild(createDiv(clock_size, 1, 2, clock_size, id+"3"));
-  odiv.appendChild(createDiv(clock_size+1, 0, clock_size, 2, id+"4"));
-  odiv.appendChild(createDiv(clock_size+1, clock_size, clock_size, 2, id+"5"));
-  odiv.appendChild(createDiv(clock_size+clock_size, 1, 2, clock_size, id+"6"));
-  return (odiv);
-}
+// function createDigit(id) {
+//   var odiv=createDiv(0, cpos, clock_size+clock_size+2, clock_size+2, false);
+//   cpos+=clock_size+4;
+//   odiv.appendChild(createDiv(0, 1, 2, clock_size, id+"0"));
+//   odiv.appendChild(createDiv(1, 0, clock_size, 2, id+"1"));
+//   odiv.appendChild(createDiv(1, clock_size, clock_size, 2, id+"2"));
+//   odiv.appendChild(createDiv(clock_size, 1, 2, clock_size, id+"3"));
+//   odiv.appendChild(createDiv(clock_size+1, 0, clock_size, 2, id+"4"));
+//   odiv.appendChild(createDiv(clock_size+1, clock_size, clock_size, 2, id+"5"));
+//   odiv.appendChild(createDiv(clock_size+clock_size, 1, 2, clock_size, id+"6"));
+//   return (odiv);
+// }
 
-function createColon(id) {
-  var odiv=createDiv(0, cpos, clock_size+clock_size+2, 2, false);
-  cpos+=4;
-  var dot1=createDiv(4, 0, 3, 2, (id)?id+"top":false);
-  dot1.style.backgroundColor=colour;
-  odiv.appendChild(dot1);
-  var dot2=createDiv(clock_size+clock_size-5, 0, 3, 2, (id)?id+"bot":false);
-  dot2.style.backgroundColor=colour;
-  odiv.appendChild(dot2);
-  return (odiv);
-}
+// function createColon(id) {
+//   var odiv=createDiv(0, cpos, clock_size+clock_size+2, 2, false);
+//   cpos+=4;
+//   var dot1=createDiv(4, 0, 3, 2, (id)?id+"top":false);
+//   dot1.style.backgroundColor=colour;
+//   odiv.appendChild(dot1);
+//   var dot2=createDiv(clock_size+clock_size-5, 0, 3, 2, (id)?id+"bot":false);
+//   dot2.style.backgroundColor=colour;
+//   odiv.appendChild(dot2);
+//   return (odiv);
+// }
 
-function createDiv(top, left, height, width, id) {
-  var div=document.createElement("div");
-  div.style.position="absolute";
-  div.style.top=top+"px";
-  div.style.left=left+"px";
-  div.style.height=height+"px";
-  div.style.width=width+"px";
-  div.style.overflow="hidden";
-  if (id) div.setAttribute("id", id);
-  return (div);
-}
+// function createDiv(top, left, height, width, id) {
+//   var div=document.createElement("div");
+//   div.style.position="absolute";
+//   div.style.top=top+"px";
+//   div.style.left=left+"px";
+//   div.style.height=height+"px";
+//   div.style.width=width+"px";
+//   div.style.overflow="hidden";
+//   if (id) div.setAttribute("id", id);
+//   return (div);
+// }
+////////
+
+// var sparks=75; // 양
+// var speed=33; // 속도
+// var bangs=5; //지속시간
+// var colours=new Array('#03f', '#f03', '#0e0', '#93f', '#0cf', '#f93', '#f0c'); 
+
+// var intensity=new Array();
+// var Xpos=new Array();
+// var Ypos=new Array();
+// var dX=new Array();
+// var dY=new Array();
+// var stars=new Array();
+// var decay=new Array();
+// var timers=new Array();
+// var swide=800;
+// var shigh=600;
+// var sleft=sdown=0;
+// var count=0;
+
+// function addLoadEvent(funky) {
+//   var oldonload=window.onload;
+//   if (typeof(oldonload)!='function') window.onload=funky;
+//   else window.onload=function() {
+//     if (oldonload) oldonload();
+//     funky();
+//   }
+// }
+
+// addLoadEvent(clicksplode);
+
+// function clicksplode() { if (document.getElementById) {
+//   var i, j;
+//   window.onscroll=set_scroll;
+//   window.onresize=set_width;
+//   document.onclick=eksplode;
+//   set_width();
+//   set_scroll();
+//   for (i=0; i<bangs; i++) for (j=sparks*i; j<sparks+sparks*i; j++) {
+//     stars[j]=createDiv('*', 13);
+//     document.body.appendChild(stars[j]);
+//   }
+// }}
+
+// function createDiv(char, size) {
+//   var div, sty;
+//   div=document.createElement('div');
+//   sty=div.style;
+//   sty.font=size+'px monospace';
+//   sty.position='absolute';
+//   sty.backgroundColor='transparent';
+//   sty.visibility='hidden';
+//   sty.zIndex='101';
+//   div.appendChild(document.createTextNode(char));
+//   return (div);
+// }
+
+// function bang(N) {
+//   var i, Z, A=0;
+//   for (i=sparks*N; i<sparks*(N+1); i++) { 
+//     if (decay[i]) {
+//       Z=stars[i].style;
+//       Xpos[i]+=dX[i];
+//       Ypos[i]+=(dY[i]+=1.25/intensity[N]);
+//       if (Xpos[i]>=swide || Xpos[i]<0 || Ypos[i]>=shigh+sdown || Ypos[i]<0) decay[i]=1;
+// 	  else {
+//         Z.left=Xpos[i]+'px';
+//         Z.top=Ypos[i]+'px';
+// 	  }
+//       if (decay[i]==15) Z.fontSize='7px';
+//       else if (decay[i]==7) Z.fontSize='2px';
+//       else if (decay[i]==1) Z.visibility='hidden';
+// 	  decay[i]--;
+// 	}
+// 	else A++;
+//   }
+//   if (A!=sparks) timers[N]=setTimeout('bang('+N+')', speed);
+// }
+
+// function eksplode(e) { 
+//   var x, y, i, M, Z, N;
+//   set_scroll();
+//   y=(e)?e.pageY:event.y+sdown;
+//   x=(e)?e.pageX:event.x+sleft;
+//   N=++count%bangs;
+//   M=Math.floor(Math.random()*3*colours.length);
+//   intensity[N]=5+Math.random()*4;
+//   for (i=N*sparks; i<(N+1)*sparks; i++) {
+//     Xpos[i]=x;
+//     Ypos[i]=y-5;
+//     dY[i]=(Math.random()-0.5)*intensity[N];
+//     dX[i]=(Math.random()-0.5)*(intensity[N]-Math.abs(dY[i]))*1.25;
+//     decay[i]=16+Math.floor(Math.random()*16);
+//     Z=stars[i].style;
+//     if (M<colours.length) Z.color=colours[i%2?count%colours.length:M];
+//     else if (M<2*colours.length) Z.color=colours[count%colours.length];
+//     else Z.color=colours[i%colours.length];
+//     Z.fontSize='13px';
+//     Z.visibility='visible';
+//   }
+//   clearTimeout(timers[N]);
+//   bang(N);
+// } 
+
+// function set_width() {
+//   var sw_min=999999;
+//   var sh_min=999999;
+//   if (document.documentElement && document.documentElement.clientWidth) {
+//     if (document.documentElement.clientWidth>0) sw_min=document.documentElement.clientWidth;
+//     if (document.documentElement.clientHeight>0) sh_min=document.documentElement.clientHeight;
+//   }
+//   if (typeof(self.innerWidth)=='number' && self.innerWidth) {
+//     if (self.innerWidth>0 && self.innerWidth<sw_min) sw_min=self.innerWidth;
+//     if (self.innerHeight>0 && self.innerHeight<sh_min) sh_min=self.innerHeight;
+//   }
+//   if (document.body.clientWidth) {
+//     if (document.body.clientWidth>0 && document.body.clientWidth<sw_min) sw_min=document.body.clientWidth;
+//     if (document.body.clientHeight>0 && document.body.clientHeight<sh_min) sh_min=document.body.clientHeight;
+//   }
+//   if (sw_min==999999 || sh_min==999999) {
+//     sw_min=800;
+//     sh_min=600;
+//   }
+//   swide=sw_min-7;
+//   shigh=sh_min-7;
+// }
+
+// function set_scroll() {
+//   if (typeof(self.pageYOffset)=='number') {
+//     sdown=self.pageYOffset;
+//     sleft=self.pageXOffset;
+//   }
+//   else if (document.body && (document.body.scrollTop || document.body.scrollLeft)) {
+//     sdown=document.body.scrollTop;
+//     sleft=document.body.scrollLeft;
+//   }
+//   else if (document.documentElement && (document.documentElement.scrollTop || document.documentElement.scrollLeft)) {
+//     sleft=document.documentElement.scrollLeft;
+//     sdown=document.documentElement.scrollTop;
+//   }
+//   else {
+//     sdown=0;
+//     sleft=0;
+//   }
+// }
+
